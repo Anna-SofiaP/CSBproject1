d369fc913b8ebf00b54a4e1e966dc42da4165ce1
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2024-05-23 13:20:10 +0300

    viimeinen haavoittuvuus tehty

--- csproject/csproject/settings.py
+++ csproject/csproject/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ttqz&!w4uckzo0@m^y3)yelanwnhhuz3$(wihp(2tsj947q)*3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = False
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    #'*'
+]
 
 
 # Application definition
